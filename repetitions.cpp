b5584f5b709884e9f04b5e7098e49e3381b29013
phfguima <phfguima@hotmail.com>
2019-03-02 17:35:10 -0300

    Adicionando terceiro desafio

---
+++ repetitions.cpp
@@ -0,0 +1,41 @@
+// Desafio 2: Repetitions
+// Link: https://cses.fi/problemset/task/1069/
+
+// Status: PASSED
+
+// Input
+// The only input line contains a string of n characters.
+
+// Output
+// Print one integer: the length of the longest repetition.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string str;
+
+    getline(cin, str);
+
+    int maior = 0;
+    int atual = 0;
+    char carac;
+    for(char& c : str) {
+        if(carac == c)
+            atual += 1;
+        else
+        {
+            if(atual >= maior)
+                maior = atual;
+            atual = 1;
+        }
+        carac = c;
+    }
+
+    if(atual > maior)
+        maior = atual;
+
+    cout << maior;
+    cout << "\n";
+}
